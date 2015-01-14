dc2e5e242200968f4d1423c47a64666a0b76d692
Eric Wedaa <wedaa@wedaa.com>
2015-01-14 10:21:20 -0500

    New Commit

---
+++ catall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Stupid program so I can cat both normal files and gzipped files
+while (( "$#" )); do
+	#echo "$1"
+	# Let cat and zcat complain about it :-)
+	#if [ ! -r $1 ] ; then
+	#	>&2 echo "Can't read $1"
+	#	exit
+	#fi
+	case $1 in
+		*gz)
+			zcat $1
+		;;
+		*)
+			cat $1
+		;;
+		esac
+	shift
+done
