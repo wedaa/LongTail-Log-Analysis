6488171988bbd4923f948be14b4106c8c58299bb
Eric Wedaa <wedaa@wedaa.com>
2015-04-12 15:13:27 -0400

    Bug Fixes

--- LongTail_dashboard.pl
+++ LongTail_dashboard.pl
@@ -6,20 +6,76 @@ $HOUR=`date +%H`;
 chomp $HOUR;
 $MINUTE=`date +%M`;
 chomp $MINUTE;
+$filedate=`date +%Y-%m-%d --date="1 day ago"`;
+#$filedate=`date +%Y-%m-%d`;
+chomp $filedate;
 
+#################################################################################
+#
+# Basically run this stuff right after midnight
+#
 if (($HOUR <1) && ($MINUTE<5)){
-	`cp /var/www/html/honey/dashboard_usernames.png /var/www/html`;
-	`cp /var/www/html/honey/dashboard_passwords.png /var/www/html`;
-	`cp /var/www/html/honey/dashboard_ips.png /var/www/html`;
-	`cp /var/www/html/honey/dashboard_number_of_attacks.png /var/www/html`;
+#if (($HOUR <12) && ($MINUTE<15)){
+	`cp /var/www/html/honey/dashboard_usernames.png /var/www/html/honey/dashboard/dashboard_usernames-$filedate.png`;
+	`cp /var/www/html/honey/dashboard_passwords.png /var/www/html/honey/dashboard/dashboard_passwords-$filedate.png`;
+	`cp /var/www/html/honey/dashboard_ips.png /var/www/html/honey/dashboard/dashboard_ips-$filedate.png`;
+	`cp /var/www/html/honey/dashboard_number_of_attacks.png /var/www/html/honey/dashboard/dashboard_number_of_attacks-$filedate.png`;
 	unlink "/var/www/html/honey/dashboard_usernames.data";
 	unlink "/var/www/html/honey/dashboard_passwords.data";
 	unlink "/var/www/html/honey/dashboard_ips.data";
 	unlink "/var/www/html/honey/dashboard_number_of_attacks.data";
+
+	open ("FILE", "/var/www/html/honey/dashboard/count") || die "Can not read to count file\n";
+	while (<FILE>){
+		chomp;
+		$count=$_;
+	}
+	close (FILE);
+	$count_plus_one =$count+1;
+	
+	#
+	# Edit the last file to point to the not yet eisting file
+	#
+	
+	open (INPUT, "/var/www/html/honey/dashboard/dashboard-$count.shtml");
+	open (OUTPUT, ">>/tmp/LongTail.$$.2");
+	while (<INPUT>){
+		if (/meta http-equiv=/){
+			print (OUTPUT "<meta http-equiv=\"refresh\" content=\"1 url=/honey/dashboard/dashboard-$count_plus_one.shtml\">\n");
+		}
+		else {
+			print (OUTPUT "$_");
+		}
+	}
+	close (INPUT);
+	close (OUTPUT);
+	system ("cp /tmp/LongTail.$$.2 /var/www/html/honey/dashboard/dashboard-$count.shtml");
+	unlink ("/tmp/LongTail.$$.2");
+	
+	open (INPUT, "/var/www/html/honey/dashboard/dashboard-$count.shtml");
+	open (OUTPUT, ">>/tmp/LongTail.$$.2");
+	while (<INPUT>){
+		if (/meta http-equiv=/){
+			print (OUTPUT "<meta http-equiv=\"refresh\" content=\"1 url=/honey/dashboard/index.shtml\">\n");
+		}
+		else {
+			$_ =~ s/\d\d\d\d-\d\d-\d\d/$filedate/;
+			print (OUTPUT "$_");
+		}
+	}
+	close (INPUT);
+	close (OUTPUT);
+	system ("cp /tmp/LongTail.$$.2 /var/www/html/honey/dashboard/dashboard-$count_plus_one.shtml");
+	unlink ("/tmp/LongTail.$$.2");
+
+	open ("FILE", ">/var/www/html/honey/dashboard/count") || die "Can not write to count file\n";
+	print (FILE "$count_plus_one\n");
+	close (FILE);
 }
 
 $DATE=`date +"%Y-%m-%d"`;
-$TIME=`date +%H:%M`;
+$TIME=`date +%k:%M`;
+$TIME =~ s/ //g;
 chomp $DATE;
 chomp $TIME;
 open (MESSAGES, "/var/log/messages")|| die "can not open /var/log/messages\n";
@@ -97,7 +153,65 @@ $count=0;
 while ($count < $lines_needed){print (FILE "0 \n");$count++;}
 close (FILE);
 
-`php /usr/local/etc/LongTail_make_graph.php /var/www/html/honey/dashboard_usernames.tmp "Unique Username Count $DATE $TIME" "" "" "wide"> /var/www/html/honey/dashboard_usernames.png`;
-`php /usr/local/etc/LongTail_make_graph.php /var/www/html/honey/dashboard_passwords.tmp "Unique Password Count $DATE $TIME" "" "" "wide"> /var/www/html/honey/dashboard_passwords.png`;
-`php /usr/local/etc/LongTail_make_graph.php /var/www/html/honey/dashboard_ips.tmp "Unique IP Count $DATE $TIME" "" "" "wide"> /var/www/html/honey/dashboard_ips.png`;
-`php /usr/local/etc/LongTail_make_graph.php /var/www/html/honey/dashboard_number_of_attacks.tmp "Number Of Attacks $DATE $TIME" "" "" "wide"> /var/www/html/honey/dashboard_number_of_attacks.png`;
+open (FILE, "/var/www/html/honey/statistics.shtml");
+while (<FILE>){
+	if (/This\ Month/){
+		chomp;
+		$_ =~ s/ //g;
+		$_ =~ s/<\/TD><TD>/|/g;
+		$_ =~ s/,//g;
+		($trash,$days,$total,$average,$std_dev,$median,$max,$min)=split (/\|/,$_);
+		last;
+	}
+}
+close (FILE);
+`php /usr/local/etc/LongTail_make_dashboard_graph.php /var/www/html/honey/dashboard_number_of_attacks.tmp "Number Of Attacks $DATE $TIME" "" "" "wide" $min $max $average> /var/www/html/honey/dashboard_number_of_attacks.png`;
+
+open (FILE, "/var/www/html/honey/more_statistics.shtml");
+$found=0;
+while (<FILE>){
+	if (/IP Address Count/){$found=1;}
+	if (($found ==1 ) && (/This\ Month/)){
+		chomp;
+		$_ =~ s/ //g;
+		$_ =~ s/<\/TD><TD>/|/g;
+		$_ =~ s/,//g;
+		($trash,$days,$total,$average,$std_dev,$median,$max,$min)=split (/\|/,$_);
+		last;
+	}
+}
+close (FILE);
+`php /usr/local/etc/LongTail_make_dashboard_graph.php /var/www/html/honey/dashboard_ips.tmp "Unique IP Count $DATE $TIME" "" "" "wide" $min $max $average > /var/www/html/honey/dashboard_ips.png`;
+
+open (FILE, "/var/www/html/honey/more_statistics.shtml");
+$found=0;
+while (<FILE>){
+	if (/Username Count/){$found=1;}
+	if (($found ==1 ) && (/This\ Month/)){
+		chomp;
+		$_ =~ s/ //g;
+		$_ =~ s/<\/TD><TD>/|/g;
+		$_ =~ s/,//g;
+		($trash,$days,$total,$average,$std_dev,$median,$max,$min)=split (/\|/,$_);
+		last;
+	}
+}
+close (FILE);
+`php /usr/local/etc/LongTail_make_dashboard_graph.php /var/www/html/honey/dashboard_usernames.tmp "Unique Username Count $DATE $TIME" "" "" "wide" $min $max $average > /var/www/html/honey/dashboard_usernames.png`;
+
+
+open (FILE, "/var/www/html/honey/more_statistics.shtml");
+$found=0;
+while (<FILE>){
+	if (/Password Count/){$found=1;}
+	if (($found ==1 ) && (/This\ Month/)){
+		chomp;
+		$_ =~ s/ //g;
+		$_ =~ s/<\/TD><TD>/|/g;
+		$_ =~ s/,//g;
+		($trash,$days,$total,$average,$std_dev,$median,$max,$min)=split (/\|/,$_);
+		last;
+	}
+}
+close (FILE);
+`php /usr/local/etc/LongTail_make_dashboard_graph.php /var/www/html/honey/dashboard_passwords.tmp "Unique Password Count $DATE $TIME" "" "" "wide" $min $max $average > /var/www/html/honey/dashboard_passwords.png`;
