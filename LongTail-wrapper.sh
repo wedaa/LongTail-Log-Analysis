394822dad0314c8a62c37bc2c91abe0ea7fed521
Eric Wedaa <wedaa@wedaa.com>
2015-05-15 18:25:09 -0400

    Newer version of wrapper script

--- LongTail-wrapper.sh
+++ LongTail-wrapper.sh
@@ -6,23 +6,26 @@ function print_divider {
 }
 
 print_divider
-date; /usr/local/etc/LongTail.sh ssh shepherd ; print_divider
-date; /usr/local/etc/LongTail.sh ssh erhp ; print_divider
-date; /usr/local/etc/LongTail.sh ssh erhp2 ; print_divider
-date; /usr/local/etc/LongTail.sh ssh syrtest ; print_divider
-date; /usr/local/etc/LongTail.sh ssh edub ; print_divider
-date; /usr/local/etc/LongTail.sh ssh edu_c ; print_divider
+#date; /usr/local/etc/LongTail.sh ssh shepherd ; print_divider
+#date; /usr/local/etc/LongTail.sh ssh erhp ; print_divider
+#date; /usr/local/etc/LongTail.sh ssh erhp2 ; print_divider
+#date; /usr/local/etc/LongTail.sh ssh syrtest ; print_divider
+#date; /usr/local/etc/LongTail.sh ssh edub ; print_divider
+#date; /usr/local/etc/LongTail.sh ssh edu_c ; print_divider
+#date; /usr/local/etc/LongTail.sh ssh blackridge ; print_divider
 date; /usr/local/etc/LongTail.sh  ; print_divider
 
-date; /usr/local/etc/LongTail.sh 2222 shepherd ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 erhp ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 erhp2 ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 syrtest ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 edub ; print_divider
+#date; /usr/local/etc/LongTail.sh 2222 shepherd ; print_divider
+#date; /usr/local/etc/LongTail.sh 2222 erhp ; print_divider
+#date; /usr/local/etc/LongTail.sh 2222 erhp2 ; print_divider
+#date; /usr/local/etc/LongTail.sh 2222 syrtest ; print_divider
+#date; /usr/local/etc/LongTail.sh 2222 edub ; print_divider
+#date; /usr/local/etc/LongTail.sh 2222 edu_c ; print_divider
+#date; /usr/local/etc/LongTail.sh 2222 blackridge ; print_divider
 date; /usr/local/etc/LongTail.sh 2222  ; print_divider
 #date; /usr/local/etc/LongTail_analyze_attacks-2222.pl ; print_divider
 
-date; /usr/local/etc/LongTail.sh telnet shepherd ; print_divider
-date; /usr/local/etc/LongTail.sh telnet erhp ; print_divider
-date; /usr/local/etc/LongTail.sh telnet erhp2 ; print_divider
-date; /usr/local/etc/LongTail.sh telnet ; print_divider
+#date; /usr/local/etc/LongTail.sh telnet shepherd ; print_divider
+#date; /usr/local/etc/LongTail.sh telnet erhp ; print_divider
+#date; /usr/local/etc/LongTail.sh telnet erhp2 ; print_divider
+#date; /usr/local/etc/LongTail.sh telnet ; print_divider
