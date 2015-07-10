a2f06b1fb404b4ef76be80fe5df0590b379dfe26
Eric Wedaa <wedaa@wedaa.com>
2015-07-09 21:26:13 -0400

    bug fix to make sure directory exists

--- get_traceroute.sh
+++ get_traceroute.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+cd /usr/local/etc
+if [ ! -d traceroute.out ] ; then
+	echo "Can not find /usr/local/etc/traceroute.out, exiting now"
+	exit
+fi
+
+
 for ip in `awk '{print $1}' ip-to-country` ; do
 echo $ip
 if [ -e traceroute.out/$ip ] ; then
--- get_whois.sh
+++ get_whois.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+cd /usr/local/etc
+if [ ! -d whois.out ] ; then
+	echo "Can not find /usr/local/etc/whois.out, exiting now"
+	exit
+fi
+
 for ip in `awk '{print $1}' ip-to-country` ; do
 echo $ip
 if [ -e whois.out/$ip ] ; then
