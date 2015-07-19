d28bd409f3eed0c139fd196278152ea5aa194a05
Eric Wedaa <wedaa@wedaa.com>
2015-07-19 19:29:54 -0400

    Added "MIDNIGHT" code so stuff is forced to do the midnight code even if it starts at 1:00 or later

--- LongTail-wrapper.sh
+++ LongTail-wrapper.sh
@@ -6,16 +6,48 @@ function print_divider {
 }
 
 print_divider
-date; /usr/local/etc/LongTail.sh ssh shepherd ; print_divider
-date; /usr/local/etc/LongTail.sh ssh erhp ; print_divider
-date; /usr/local/etc/LongTail.sh ssh erhp2 ; print_divider
-date; /usr/local/etc/LongTail.sh ssh syrtest ; print_divider
-date; /usr/local/etc/LongTail.sh ssh edub ; print_divider
-date; /usr/local/etc/LongTail.sh ssh edu_c ; print_divider
-date; /usr/local/etc/LongTail.sh ssh blackridge ; print_divider
-date; /usr/local/etc/LongTail.sh  ; print_divider
-date; grep blackridge /var/log/messages |egrep -v systemd-logind\|dbus\|\ su: |sed 's/Password:..*$/Password: XXXX/' > /var/www/html/honey/blackridge/current-raw-data.txt
-date; grep blackridge /var/log/messages |egrep -v systemd-logind\|dbus\|\ su: |sed 's/Password:..*$/Password: XXXX/'|grep 'blackridge sshd\[' > /var/www/html/honey/blackridge/current-identified-traffic.txt
+START_HOUR=`date +%H` # This is used at the end of the program but we want to know it NOW
+if [ $START_HOUR -eq 0 ]; then
+	date; /usr/local/etc/LongTail.sh 2222  ; print_divider # This runs first since it's fastest
+	date; /usr/local/etc/LongTail.sh  ; print_divider # This should still run at midnight
+	date; /usr/local/etc/LongTail.sh ssh blackridge MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh erhp MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh erhp2 MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh shepherd MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh syrtest MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh edub MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh edu_c MIDNIGHT ; print_divider
+
+	date; /usr/local/etc/LongTail.sh 2222 shepherd MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 erhp MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 erhp2 MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 syrtest MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 edub MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 edu_c MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 blackridge MIDNIGHT ; print_divider
+else
+	date; /usr/local/etc/LongTail.sh  ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh blackridge ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh erhp ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh erhp2 ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh shepherd ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh syrtest ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh edub ; print_divider
+	date; /usr/local/etc/LongTail.sh ssh edu_c ; print_divider
+
+	date; /usr/local/etc/LongTail.sh 2222  ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 shepherd ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 erhp ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 erhp2 ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 syrtest ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 edub ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 edu_c ; print_divider
+	date; /usr/local/etc/LongTail.sh 2222 blackridge ; print_divider
+fi
+
+
+date; grep blackridge /var/log/messages |egrep -v systemd-logind\|dbus\|\ su: |sed 's/Password:..*$/Password: XXXX/' |sed 's/ RSA..*$//' > /var/www/html/honey/blackridge/current-raw-data.txt
+date; grep blackridge /var/log/messages |egrep -v systemd-logind\|dbus\|\ su: |sed 's/Password:..*$/Password: XXXX/'|grep 'blackridge sshd\[' |sed 's/ RSA..*$//' > /var/www/html/honey/blackridge/current-identified-traffic.txt
 ONE_AM=1
 START_HOUR=`date +%H` # This is used at the end of the program but we want to know it NOW
 if [ $START_HOUR -eq $ONE_AM ]; then
@@ -28,14 +60,6 @@ if [ $START_HOUR -eq $ONE_AM ]; then
 fi
 
 
-date; /usr/local/etc/LongTail.sh 2222 shepherd ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 erhp ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 erhp2 ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 syrtest ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 edub ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 edu_c ; print_divider
-date; /usr/local/etc/LongTail.sh 2222 blackridge ; print_divider
-date; /usr/local/etc/LongTail.sh 2222  ; print_divider
 #date; /usr/local/etc/LongTail_analyze_attacks-2222.pl ; print_divider
 
 #date; /usr/local/etc/LongTail.sh telnet shepherd ; print_divider
