0e80dc127fe33a7bcb9650ad9e31164c1f0dfb2b
Eric Wedaa <wedaa@wedaa.com>
2015-08-13 17:47:31 -0400

    More cleanup

--- install.sh
+++ install.sh
@@ -2,12 +2,21 @@
 
 echo "You need to edit LongTail.sh.  Please see LongTail.sh for details"
 echo ""
-echo "You need to edit this file for SCRIPT_DIR, HTML_DIR, DICT_DIR, and OWNER."
+echo "You need to edit this file for OWNER."
 echo ""
 echo "And then comment out the exit statement"
 
-exit
+#exit
 
+OWNER="wedaa" # What is the owner of the process running LongTail?
+
+# DO NOT EDIT BELOW THIS LINE
+# I'm sorry, there is still stuff hard-coded in the programs
+# that reference these locations.
+echo ""
+echo "#############################################################"
+echo "Checking for OS"
+echo ""
 RHEL=0
 
 if [ -e /etc/redhat-release ] ; then
@@ -22,6 +31,11 @@ if [ -e /etc/centos-release ] ; then
 	echo "This seems to be a RedHat/Centos based system"
 	RHEL=1
 fi
+echo ""
+echo ""
+echo "#############################################################"
+echo "Checking for which command"
+echo ""
 
 which ls >/dev/null 2>&1
 LAST=$?
@@ -64,14 +78,43 @@ HTML_DIR="/var/www/html/honey" # Where do we put the HTML files?
 DICT_DIR="/usr/local/dict"
 BOTS_DIR="/usr/local/etc/LongTail_botnets"
 
-OWNER="longtail"                  # What is the owner of the process running LongTail?
+echo ""
+echo "#############################################################"
+echo "Checking to see that $OWNER already exists"
+echo ""
+
+echo ""
+if id -u "$OWNER" >/dev/null 2>&1; then
+	echo "user $OWNER exists, this is good"
+else
+	echo "user $OWNER does not exist, this is bad"
+	echo "Please create the user and re-run this script"
+	echo ""
+	exit
+fi
+echo ""
+echo "#############################################################"
+echo "Making $HTML_DIR/historical/`date +%Y`/`date +%m`/`date +%d` now"
+echo ""
 
 mkdir -p $HTML_DIR/historical/`date +%Y`/`date +%m`/`date +%d`
-if [ ! -d "$HTML_DIR/dashboard" ] ; then
-	mkdir $HTML_DIR/dashboard
-fi
+chown $OWNER $HTML_DIR/historical/`date +%Y`/`date +%m`/`date +%d`
+chmod a+rx $HTML_DIR/historical/`date +%Y`/`date +%m`/`date +%d`
+chown $OWNER $HTML_DIR/historical/`date +%Y`/`date +%m`
+chmod a+rx $HTML_DIR/historical/`date +%Y`/`date +%m`
+chown $OWNER $HTML_DIR/historical/`date +%Y`
+chmod a+rx $HTML_DIR/historical/`date +%Y`
+chown $OWNER $HTML_DIR/historical
+chmod a+rx $HTML_DIR/historical
+chown $OWNER $HTML_DIR
+chmod a+rx $HTML_DIR
 
-OTHER_DIRS="/usr/local/etc/black_lists /var/www/html/honey-2222 /var/www/html/honey-22 /var/www/html/telnet /var/www/html/ftp /var/www/html/rlogin"
+echo ""
+echo "#############################################################"
+echo "Making other dirs now"
+echo ""
+
+OTHER_DIRS="$HTML_DIR/dashboard /usr/local/etc/black_lists /var/www/html/honey-2222 /var/www/html/honey-22 /var/www/html/telnet /var/www/html/ftp /var/www/html/rlogin"
 
 for dir in $SCRIPT_DIR $HTML_DIR  $DICT_DIR $OTHER_DIRS $BOTS_DIR; do
 	if [ -e $dir ] ; then
@@ -97,10 +140,15 @@ if [ ! -d $SCRIPT_DIR/LongTail_local_reports ] ; then
 	else
 		mkdir $SCRIPT_DIR/LongTail_local_reports
 		chown $OWNER $SCRIPT_DIR/LongTail_local_reports
+		chmod a+rx $OWNER $SCRIPT_DIR/LongTail_local_reports
 	fi
 fi
 
-DICT_FILES="wordsEn.txt"
+
+echo ""
+echo "#############################################################"
+echo "Copying bots files now"
+echo ""
 
 BOTS_FILES="big_botnet \
 LongTail_find_botnet.pl  \
@@ -119,10 +167,15 @@ small_bots_3"
 
 
 for file in $BOTS_FILES ; do
-	echo $file
+	#echo $file
 	cp LongTail_botnets/$file $BOTS_DIR
 	chmod a+r $BOTS_DIR/$file
+	chown $OWNER $BOTS_DIR/$file
 done
+echo ""
+echo "#############################################################"
+echo "Copying assorted /usr/local/etc/ ip related files now"
+echo ""
 
 ETC_FILES="ip-to-country \
 translate_country_codes.sed \
@@ -138,11 +191,16 @@ LongTail_sshPsycho_IP_addresses"
 
 
 for file in $ETC_FILES ; do
-	echo $file
+#	echo $file
 	cp $file $SCRIPT_DIR
 	chmod a+r $SCRIPT_DIR/$file
+	chown $OWNER $SCRIPT_DIR/$file
 done
 
+echo ""
+echo "#############################################################"
+echo "Copying assorted /usr/local/etc/ programs now"
+echo ""
 PROGRAMS=" LongTail_rebuild_dashboard_index.pl \
 LongTail_find_badguys_looking_for_passwords.sh \
 LongTail_make_map.pl \
@@ -181,17 +239,28 @@ LongTail_find_ssh_probers.pl \
 whois.pl "
 
 for file in $PROGRAMS ; do
-	echo $file
+#	echo $file
 	cp $file $SCRIPT_DIR
 	chmod a+rx $SCRIPT_DIR/$file
+	chown $OWNER $SCRIPT_DIR/$file
 done
 
+echo ""
+echo "#############################################################"
+echo "Copying assorted dictionary files now"
+echo ""
+DICT_FILES="wordsEn.txt"
 for file in $DICT_FILES ; do
 	echo $file
 	cp $file $DICT_DIR
 	chmod a+r $DICT_DIR/$file
+	chown $OWNER $DICT_DIR/$file
 done
 
+echo ""
+echo "#############################################################"
+echo "Copying assorted html files now"
+echo ""
 HTML_FILES="ip_addresses.shtml \
 index.shtml \
 last-30-days-map.html \
@@ -219,64 +288,41 @@ for dir in $HTML_DIR $OTHER_DIRS ; do
 	for file in $HTML_FILES ; do
 		cp $file $dir
 		chmod a+r $dir/$file	
+		chown $OWNER $dir/$file	
 	done
 done
 
+echo ""
+echo "#############################################################"
+echo "Copying assorted html files for port 2222 tests now"
+echo ""
 cp honey-2222/* /var/www/html/honey-2222/
 chmod a+r /var/www/html/honey-2222/*
+chown -R $OWNER /var/www/html/honey-2222/*
 
+echo ""
+echo "#############################################################"
+echo "Setting up dashboard files"
+echo ""
 cp dashboard-index.shtml $HTML_DIR/dashboard/index.shtml
 cp dashboard-1.shtml $HTML_DIR/dashboard/
 cp dashboard.shtml $HTML_DIR/
 
 echo "0" > $HTML_DIR/dashboard/count
 
-if [ ! -e $SCRIPT_DIR/LongTail-exclude-accounts.grep ] ; then
-	echo "LongTail-exclude-accounts.grep not in $SCRIPT_DIR"
-	cp LongTail-exclude-accounts.grep $SCRIPT_DIR
-fi
-
-if [ ! -e $SCRIPT_DIR/LongTail-exclude-IPs-httpd.grep ] ; then
-	echo "LongTail-exclude-IPs-httpd.grep not in $SCRIPT_DIR"
-	cp LongTail-exclude-IPs-httpd.grep $SCRIPT_DIR
-fi
-
-if [ ! -e $SCRIPT_DIR/LongTail-exclude-IPs-ssh.grep ] ; then
-	echo "LongTail-exclude-IPs-ssh.grep not in $SCRIPT_DIR"
-	cp LongTail-exclude-IPs-ssh.grep $SCRIPT_DIR
-fi
-
-if [ ! -e $SCRIPT_DIR/LongTail-exclude-webpages.grep ] ; then
-	echo "LongTail-exclude-webpages.grep not in $SCRIPT_DIR"
-	cp LongTail-exclude-webpages.grep $SCRIPT_DIR
-fi
+for file in $HTML_DIR/dashboard/index.shtml \
+	$HTML_DIR/dashboard/dashboard-1.shtml \
+	$HTML_DIR/dashboard.shtml \
+	$HTML_DIR/dashboard/count ; do
+	chown $OWNER $file
+	chmod a+r $file
+done
 
 
-chmod a+r $HTML_DIR/index.shtml
-chown $OWNER $HTML_DIR/index.shtml
-chmod a+r $HTML_DIR/index-long.shtml
-chown $OWNER $HTML_DIR/index-long.shtml
-chmod a+r $HTML_DIR/index-historical.shtml
-chown $OWNER $HTML_DIR/index-historical.shtml
-chmod a+r $HTML_DIR/graphics.shtml
-chown $OWNER $HTML_DIR/graphics.shtml
-chmod a+r $HTML_DIR/graphics.shtml
-chown $OWNER $HTML_DIR/graphics.shtml
-
-chmod a+r $HTML_DIR/LongTail.css
-chown $OWNER $HTML_DIR/LongTail.css
-chmod a+r $HTML_DIR/header.html
-chown $OWNER $HTML_DIR/header.html
-chmod a+r $HTML_DIR/footer.html
-chown $OWNER $HTML_DIR/footer.html
-
-touch $SCRIPT_DIR/Longtail-ssh-local-reports
-touch $SCRIPT_DIR/Longtail-httpd-local-reports
-
-#
-# Make a default file THISYEAR/THISMONTH/TODAY/current-attack-count.data
-# To clear a divide by 0 error that shows up on the fist day running it.
-
+echo ""
+echo "#############################################################"
+echo "Setting up historical data files"
+echo ""
 
 YEAR=`date +%Y`
 MONTH=`date +%m`
@@ -286,29 +332,29 @@ mkdir -p $HTML_DIR/historical/$YEAR/$MONTH/$DAY
 echo "0" > $HTML_DIR/historical/$YEAR/$MONTH/$DAY/current-attack-count.data
 chown -R $OWNER $HTML_DIR/historical
 chmod a+rx $HTML_DIR $HTML_DIR/historical $HTML_DIR/historical/$YEAR $HTML_DIR/historical/$YEAR/$MONTH $HTML_DIR/historical/$YEAR/$MONTH/$DAY $HTML_DIR/historical/$YEAR/$MONTH/$DAY/current-attack-count.data
-
-#
-# Lets deal with the tour
-#
-mkdir -p $HTML_DIR/tour
-chmod a+rx $HTML_DIR/tour
-cd tour
-cp * $HTML_DIR/tour
-chmod a+r $HTML_DIR/tour/*
-cd ..
+chown $OWNER $HTML_DIR $HTML_DIR/historical $HTML_DIR/historical/$YEAR $HTML_DIR/historical/$YEAR/$MONTH $HTML_DIR/historical/$YEAR/$MONTH/$DAY $HTML_DIR/historical/$YEAR/$MONTH/$DAY/current-attack-count.data
 
 #
 # Lets deal with the LongTail_local_reports
 #
+echo ""
+echo "#############################################################"
+echo "Copying LongTail_local_reports/* now"
+echo ""
 cd LongTail_local_reports
 cp * $SCRIPT_DIR/LongTail_local_reports
 chmod a+rx $SCRIPT_DIR/LongTail_local_reports/*
+chown $OWNER $SCRIPT_DIR/LongTail_local_reports/*
 cd ..
 
 
 #
 # Check for required software here
 #
+echo ""
+echo "#############################################################"
+echo "Checking for required programs now"
+echo ""
 for i in tar perl php find sort uniq grep egrep cat tac unzip bzcat zcat whois ; do
 	echo -n "Checking for $i...  "
 	which $i >/dev/null
@@ -319,16 +365,27 @@ for i in tar perl php find sort uniq grep egrep cat tac unzip bzcat zcat whois ;
 	fi
 done
 
+echo ""
+echo "#############################################################"
+echo "Installing jpgraph now"
+echo ""
+
 if [ ! -d /usr/local/php/jpgraph-3.5.0b1 ] ; then
 		mkdir -p /usr/local/php
 		cp jpgraph-3.5.0b1.tar.gz /usr/local/php
 		pushd /usr/local/php
-		tar -xvf /usr/local/php
+		tar -xf /usr/local/php/jpgraph-3.5.0b1.tar.gz
+		chown -R $OWNER /usr/local/php/jpgraph-3.5.0b1
+		find /usr/local/php/jpgraph-3.5.0b1 -type d |xargs chmod a+rx
 		popd
 else
 	echo "It appears that /usr/local/php/jpgraph-3.5.0b1 already "
 	echo "exists, not installing it again."
 fi
+echo ""
+echo "#############################################################"
+echo "Installing fancybox now"
+echo ""
 
 if [ ! -d /var/www/html/honey/fancybox ] ; then
 		mkdir -p /var/www/html/honey/fancybox
@@ -339,6 +396,9 @@ else
 	echo "exists, not installing it again."
 fi
 
+echo ""
+echo "#############################################################"
+echo ""
 
 echo "You should probably run the following command to install all the php "
 echo "required for graphing"
@@ -347,21 +407,28 @@ echo "            OR"
 echo "       yum install whois php php-devel php-common php-cli php-xml php-pear php-pdo php-gd (RHEL 7)"
 echo ""
 echo "Don't forget to edit the include line in "
-echo "/etc/php.ini to reference /usr/local/php)."
+echo "/etc/php.ini to reference /usr/local/php."
 
 echo ""
 echo "You need to install truetype fonts by hand.  Please see the README for directions"
 echo ""
+echo ""
+echo "#############################################################"
+echo ""
 
 echo ""
-echo "Please add these entries to your crontab file"
+echo "Please add the entries from sample.crontab to your crontab file"
 echo ""
-cat sample.crontab
 
+echo ""
+echo "#############################################################"
+echo ""
 echo ""
 echo "Please run "
 echo "      /usr/local/etc/LongTail.sh "
-echo "and"
-echo "      /usr/local/etc/LongTail.sh MIDNIGHT"
+#echo "and"
+#echo "      /usr/local/etc/LongTail.sh MIDNIGHT"
 echo "to both test your installation, and to finish creating all the webpages"
+echo ""
+echo "#############################################################"
 echo ""
