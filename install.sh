84c7c76a32c762c37ea81f77de59f9c738c7d551
Eric Wedaa <wedaa@wedaa.com>
2016-02-03 17:01:27 -0500

    Fixed bug where I did a 'cd' instead of a 'pushd' and lost my way, resulting in an incomplete installation

--- install.sh
+++ install.sh
@@ -406,13 +406,14 @@ cpan Geo::IP
 cpan Socket6
 mkdir /usr/local/share/GeoIP
 mkdir /usr/local/share/GeoIP/backups # Will be used to store older copies of database files
-cd /usr/local/share/GeoIP
+pushd /usr/local/share/GeoIP
 wget geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
 wget geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 wget http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz
 gunzip -f GeoIP.dat.gz
 gunzip -f GeoLiteCity.dat.gz
 gunzip -f GeoIPv6.dat.gz
+popd
 
 
 echo ""
