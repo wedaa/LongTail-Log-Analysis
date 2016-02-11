2442dc80d80cee2283ab0ada9887a44738891042
Eric Wedaa <wedaa@wedaa.com>
2016-02-11 17:58:47 -0500

    general cleanup plus examples of the new parameters

--- LongTail-wrapper.sh
+++ LongTail-wrapper.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+# This is an example wrapper program for you to use if you
+# are monitoring more than just ssh and/or want to break out
+# statistics by server.
+#
+# This example monitors all hosts, and then breaks out statistics
+# for hosts named erhp, erhp2, and shepherd
+#
+# Please note the use of "-midnight" in the section for $START_HOUR -eq 0 
+# This is because when you have 60 million data points, the midnight
+# scripts can take more than an hour running on LongTail V1.5.  This
+# will be fixed in LongTail V2.0
+
 function print_divider {
 	echo ""
 	echo "================================================================"
@@ -9,60 +21,26 @@ print_divider
 START_HOUR=`date +%H` # This is used at the end of the program but we want to know it NOW
 if [ $START_HOUR -eq 0 ]; then
 	echo "It's MIDNIGHT, running midnight scripts"
-	date; /usr/local/etc/LongTail.sh http  ; print_divider # This runs first since it's fastest
-	date; /usr/local/etc/LongTail.sh 2222  ; print_divider # This runs first since it's fastest
-	date; /usr/local/etc/LongTail.sh  ; print_divider # This should still run at midnight
-	date; /usr/local/etc/LongTail.sh ssh blackridge MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh erhp MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh erhp2 MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh shepherd MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh syrtest MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh edub MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh edu_c MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol http -midnight ; print_divider # This runs first since it's fastest
+	date; /usr/local/etc/LongTail.sh -protocol 2222 -midnight ; print_divider # This runs first since it's fastest
+	date; /usr/local/etc/LongTail.sh -protocol ssh  ; print_divider # This should still run at midnight
+	date; /usr/local/etc/LongTail.sh -protocol ssh -host erhp -midnight ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol ssh -host erhp2 -midnight ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol ssh -host shepherd -midnight ; print_divider
 
-	date; /usr/local/etc/LongTail.sh 2222 shepherd MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 erhp MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 erhp2 MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 syrtest MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 edub MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 edu_c MIDNIGHT ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 blackridge MIDNIGHT ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol 2222 -host shepherd -midnight ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol 2222 -host erhp -midnight ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol 2222 -host erhp2 -midnight ; print_divider
 else
-	date; /usr/local/etc/LongTail.sh http ; print_divider
-	date; /usr/local/etc/LongTail.sh  ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh blackridge ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh erhp ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh erhp2 ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh shepherd ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh syrtest ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh edub ; print_divider
-	date; /usr/local/etc/LongTail.sh ssh edu_c ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol http ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol ssh ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol ssh -host erhp ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol ssh -host erhp2 ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol ssh -host shepherd ; print_divider
 
-	date; /usr/local/etc/LongTail.sh 2222  ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 shepherd ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 erhp ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 erhp2 ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 syrtest ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 edub ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 edu_c ; print_divider
-	date; /usr/local/etc/LongTail.sh 2222 blackridge ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol 2222  ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol 2222 -host shepherd ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol 2222 -host erhp ; print_divider
+	date; /usr/local/etc/LongTail.sh -protocol 2222 -host erhp2 ; print_divider
 fi
 
-
-date; grep blackridge /var/log/messages |egrep -v systemd-logind\|dbus\|\ su: |sed 's/Password:..*$/Password: XXXX/' |sed 's/ RSA..*$//' > /var/www/html/honey/blackridge/current-raw-data.txt
-date; grep blackridge /var/log/messages |egrep -v systemd-logind\|dbus\|\ su: |sed 's/Password:..*$/Password: XXXX/'|grep 'blackridge sshd\[' |sed 's/ RSA..*$//' > /var/www/html/honey/blackridge/current-identified-traffic.txt
-ONE_AM=1
-START_HOUR=`date +%H` # This is used at the end of the program but we want to know it NOW
-if [ $START_HOUR -eq $ONE_AM ]; then
-	YESTERDAY_YEAR=`date  +"%Y" --date="1 day ago"`
-	YESTERDAY_MONTH=`date  +"%m" --date="1 day ago"`
-	YESTERDAY_DAY=`date  +"%d" --date="1 day ago"`
-
-	grep blackridge /var/log/messages* |grep $YESTERDAY_YEAR-$YESTERDAY_MONTH-$YESTERDAY_DAY |egrep -v systemd-logind\|dbus\|\ su: |sed 's/Password:..*$/Password: XXXX/'|grep 'blackridge sshd\[' > /var/www/html/honey/blackridge/historical/$YESTERDAY_YEAR/$YESTERDAY_MONTH/$YESTERDAY_DAY/identified_traffic.data
-	cat /var/www/html/honey/blackridge/historical/$YESTERDAY_YEAR/$YESTERDAY_MONTH/$YESTERDAY_DAY/identified_traffic.data |grep Accepted\ publickey\ for\ wedaa |wc -l > /var/www/html/honey/blackridge/historical/$YESTERDAY_YEAR/$YESTERDAY_MONTH/$YESTERDAY_DAY/identified_traffic.data.count
-fi
-
-#date; /usr/local/etc/LongTail.sh telnet shepherd ; print_divider
-#date; /usr/local/etc/LongTail.sh telnet erhp ; print_divider
-#date; /usr/local/etc/LongTail.sh telnet erhp2 ; print_divider
-#date; /usr/local/etc/LongTail.sh telnet ; print_divider
