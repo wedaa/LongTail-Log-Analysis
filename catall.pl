72522613a0e613853e57d9313b19260bfd8e90a4
Eric Wedaa <wedaa@wedaa.com>
2016-08-24 16:55:56 -0400

    added more paranoia

--- catall.pl
+++ catall.pl
@@ -2,30 +2,6 @@
 # Stupid program so I can cat both normal files and gzipped files
 #
 
-#while (( "$#" )); do
-#	#echo "$1"
-#	# Let cat and zcat complain about it :-)
-#	#if [ ! -r $1 ] ; then
-#	#	>&2 echo "Can't read $1"
-#	#	exit
-#	#fi
-#	case $1 in
-#		*bz2)
-#			bzcat $1 |sed 's/|/BAR/g'|sed 's/\\/BACKSLASH/g'|sed 's/&/AMPERSAND/g' |sed 's/wget/WGET/g' |sed 's/tftp/TFTP/g'|sed 's/curl/CURL/g' |sed 's/ftpget/FTPGET/g' |sed 's/ftp/FTP/'|sed 's/busybox/BUSYBOX/g'
-#		;;
-#		*bz)
-#			bzcat $1 |sed 's/|/BAR/g'|sed 's/\\/BACKSLASH/g'|sed 's/&/AMPERSAND/g' |sed 's/wget/WGET/g' |sed 's/tftp/TFTP/g'|sed 's/curl/CURL/g' |sed 's/ftpget/FTPGET/g' |sed 's/ftp/FTP/'|sed 's/busybox/BUSYBOX/g'
-#		;;
-#		*gz)
-#			zcat $1 |sed 's/|/BAR/g'|sed 's/\\/BACKSLASH/g'|sed 's/&/AMPERSAND/g' |sed 's/wget/WGET/g' |sed 's/tftp/TFTP/g'|sed 's/curl/CURL/g' |sed 's/ftpget/FTPGET/g' |sed 's/ftp/FTP/'|sed 's/busybox/BUSYBOX/g'
-#		;;
-#		*)
-#			cat $1 |sed 's/|/BAR/g'|sed 's/\\/BACKSLASH/g'|sed 's/&/AMPERSAND/g' |sed 's/wget/WGET/g' |sed 's/tftp/TFTP/g'|sed 's/curl/CURL/g' |sed 's/ftpget/FTPGET/g' |sed 's/ftp/FTP/'|sed 's/busybox/BUSYBOX/g'
-#		;;
-#		esac
-#	shift
-#done
-
 while ($filename =shift){
 print "$filename\n";
 	$command = "cat ";
@@ -47,6 +23,8 @@ print "$filename\n";
 			$_ =~ s/ftp/FTP/g;
 			$_ =~ s/tftp/TFTP/g;
 			$_ =~ s/busybox/BUSYBOX/g;
+			$_ =~ s/rm /RM /g;
+			$_ =~ s/cd /CD /g;
 		}
 		print;
 	}
